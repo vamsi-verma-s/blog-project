f9b1cf83ebe7f597e2b94f7cad0a98b670524818
Vamsi Verma <vamsi190294@gmail.com>
2019-09-14 14:30:11 +0530

    commit before adding comments

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class EmailPostForm(froms.Form):
+class EmailPostForm(forms.Form):
     name = forms.CharField( max_length= 25)
     email = forms.EmailField()
     to = forms.EmailField()
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 # Create your views here.
 
 def post_list(request):
@@ -36,6 +37,7 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
     # Retrieve post by id 
     post = get_object_or_404(Post, id=post_id , status='published')
+    sent = False
 
     if request.method == 'POST':
         # Form was submitted
@@ -44,11 +46,20 @@ def post_share(request, post_id):
             # Form fields passed validation
             cd = form.cleaned_data
             # ...send email
-
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'],
+                                                                    cd ['email'],
+                                                                    post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title,
+                                                                    post_url,
+                                                                    cd['name'],
+                                                                    cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {
-        'post': post, 'form':form
+        'post': post, 'form':form, 'sent':sent
     })
 
 
