cf2ce92c59c9cd189b5b8d5b86f38ca6be0049a5
Vamsi Verma <vamsi190294@gmail.com>
2019-09-14 15:50:35 +0530

    Added Comments model and registered in admin

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -50,3 +50,20 @@ class Post(models.Model):
         return self.title
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                            on_delete=models.CASCADE,
+                            )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
+
