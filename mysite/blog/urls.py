8b596bef1ad8b4f448eef37ea37425b52c744ab2
Vamsi Verma <vamsi190294@gmail.com>
2019-09-15 16:24:42 +0530

    Added the sitemap farmework, feed syndication farmework

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
 urlpatterns = [
     # post views
     path('', views.post_list, name='post_list'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
             views.post_detail,
